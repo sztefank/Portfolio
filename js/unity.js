b43eb7f90d40284ff074bb0692fac25751e5fd0b
zVyxx <sztefankracsica0@gmail.com>
2025-07-15 20:54:32 +0200

    Add files

---
+++ js/unity.js
@@ -0,0 +1,27 @@
+﻿let unityInstance = null;
+
+window.startUnity = () => {
+    if (unityInstance) {
+        console.log("Unity läuft bereits.");
+        return;
+    }
+    createUnityInstance(document.querySelector("#unityContainer"), {
+        dataUrl: "unitygame/Build/SlendermanBuild.data",
+        frameworkUrl: "unitygame/Build/SlendermanBuild.framework.js",
+        codeUrl: "unitygame/Build/SlendermanBuild.wasm"
+    }).then(instance => {
+        unityInstance = instance;
+    });
+};
+
+window.stopUnityCompletely = async () => {
+    if (unityInstance) {
+        await unityInstance.Quit();
+        unityInstance = null;
+    }
+    const canvas = document.getElementById("unityContainer");
+    if (canvas) {
+        canvas.remove();
+    }
+    console.log("Unity wurde vollständig beendet und Canvas entfernt.");
+};
